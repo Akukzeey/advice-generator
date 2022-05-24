91b4963d5ea0ee3472d75995b11d6c064370bb21
Farouk <thiagoacookie@gmail.com>
2022-05-24 18:43:04 +0300

    Advice generator Js

--- app.js
+++ app.js
@@ -6,13 +6,13 @@ dice.addEventListener('click',(e)=>{
         printAdvice()
 })
 
-
 function printAdvice(){
    axios.get("https://api.adviceslip.com/advice").then((resolve)=>{
          const {id,advice}=resolve.data.slip
         let ad=advice
         let num=id
-
+        paragraph.textContent=ad
+        num.textContent=num
     }).catch(err=>{
         console.log('an error thrown',err)
     })
